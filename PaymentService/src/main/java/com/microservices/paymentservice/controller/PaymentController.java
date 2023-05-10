ea2bac95108cb0e9ab35cef89eb3ddcd16cafa97
Yinon Tzomi <92912589+YinonTzo@users.noreply.github.com>
2023-05-10 20:31:49 +0300

    Implement do payment and calling to it from order service.

--- PaymentService/src/main/java/com/microservices/paymentservice/controller/PaymentController.java
+++ PaymentService/src/main/java/com/microservices/paymentservice/controller/PaymentController.java
@@ -1,9 +1,22 @@
 package com.microservices.paymentservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.microservices.paymentservice.entity.TransactionDetails;
+import com.microservices.paymentservice.model.PaymentRequest;
+import com.microservices.paymentservice.service.PaymentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payment")
+@AllArgsConstructor
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<TransactionDetails> doPayment(@RequestBody PaymentRequest paymentRequest) {
+        return new ResponseEntity<>(paymentService.doPayment(paymentRequest), HttpStatus.OK);
+    }
 }
