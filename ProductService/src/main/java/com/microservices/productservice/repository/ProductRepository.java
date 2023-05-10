a1eebae0a810aa7d257c654be991961c42a5ac88
Yinon Tzomi <92912589+YinonTzo@users.noreply.github.com>
2023-05-10 17:37:27 +0300

    Add repository annotation.

--- ProductService/src/main/java/com/microservices/productservice/repository/ProductRepository.java
+++ ProductService/src/main/java/com/microservices/productservice/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.microservices.productservice.repository;
 
 import com.microservices.productservice.entity.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 }
