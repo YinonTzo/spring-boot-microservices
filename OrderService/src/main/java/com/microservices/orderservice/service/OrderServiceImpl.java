6941491522c2a1b70d0a2d83ad7278de11115b31
Yinon Tzomi <92912589+YinonTzo@users.noreply.github.com>
2023-05-16 19:54:50 +0300

    Add circuit breaker

--- OrderService/src/main/java/com/microservices/orderservice/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/microservices/orderservice/service/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import com.microservices.orderservice.mappers.OrderMapper;
 import com.microservices.orderservice.model.*;
 import com.microservices.orderservice.repository.OrderRepository;
 import com.microservices.orderservice.response.ErrorResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
@@ -13,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import reactor.core.publisher.Mono;
 
 //TODO: check how to implement the web client correctly. Maybe create some caller object.
@@ -26,11 +28,53 @@ public class OrderServiceImpl implements OrderService {
     private final WebClient webClient;
 
     @Override
+    @CircuitBreaker(name = "product-service", fallbackMethod = "fallback")
     public Order placeOrder(OrderRequest orderRequest) {
-
         log.info("Placing order request {}.", orderRequest);
 
-        log.info("Calling product service to reduce quantity.");
+        log.info("Calling the product service to reduce quantity.");
+        reduceQuantityInProductService(orderRequest);
+
+        log.info("Creating order with status CREATED.");
+        Order order = OrderMapper.INSTANCE.orderRequestToOrder(orderRequest);
+
+        Order savedOrder = orderRepository.save(order);
+
+        log.info("Calling the payment service to complete the payment.");
+        PaymentRequest paymentRequest = PaymentRequest.builder()
+                .orderId(order.getId())
+                .paymentMode(orderRequest.getPaymentMode())
+                .amount(orderRequest.getAmount())
+                .build();
+
+        payInPaymentService(paymentRequest);
+        //TODO: handle rollback for the reduce quantity. If the payment fails the quantity will return to the old quantity.
+
+        log.info("Payment done Successfully. Changing the Oder status to PLACED");
+        order.setOrderStatus("PLACED");
+        orderRepository.save(order);
+
+        log.info("Order placed successfully {}.", savedOrder);
+        return order;
+    }
+
+    private void payInPaymentService(PaymentRequest paymentRequest) {
+        webClient.post()
+                .uri("http://localhost:8083/payment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(paymentRequest))
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> clientResponse.bodyToMono(ErrorResponse.class)
+                        .flatMap(errorBody -> Mono.error(new OrderServiceCustomException(
+                                errorBody.getErrorMessage(),
+                                errorBody.getErrorCode(),
+                                clientResponse.statusCode().value()
+                        ))))
+                .toBodilessEntity()
+                .block();
+    }
+
+    private void reduceQuantityInProductService(OrderRequest orderRequest) {
         webClient.put()
                 .uri(uriBuilder -> uriBuilder
                         .scheme("http")
@@ -48,44 +92,10 @@ public class OrderServiceImpl implements OrderService {
                         ))))
                 .toBodilessEntity()
                 .block();
-
-
-        log.info("Creating order with status CREATED.");
-        Order order = OrderMapper.INSTANCE.orderRequestToOrder(orderRequest);
-
-        Order savedOrder = orderRepository.save(order);
-
-        log.info("Calling payment service to complete the payment.");
-        PaymentRequest paymentRequest = PaymentRequest.builder()
-                .orderId(order.getId())
-                .paymentMode(orderRequest.getPaymentMode())
-                .amount(orderRequest.getAmount())
-                .build();
-
-        webClient.post()
-                .uri("http://localhost:8083/payment")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromValue(paymentRequest))
-                .retrieve()
-                //TODO: handle exception and rollback for the reduce quantity. If the payment fails the quantity will return to the old quantity.
-                /*.onStatus(HttpStatusCode::isError, clientResponse -> clientResponse.bodyToMono(ErrorResponse.class)
-                        .flatMap(errorBody -> Mono.error(new OrderServiceCustomException(
-                                errorBody.getErrorMessage(),
-                                errorBody.getErrorCode(),
-                                clientResponse.statusCode().value()
-                        ))))*/
-                .toBodilessEntity()
-                .block();
-
-        log.info("Payment done Successfully. Changing the Oder status to PLACED");
-        order.setOrderStatus("PLACED");
-        orderRepository.save(order);
-
-        log.info("Order placed successfully {}.", savedOrder);
-        return order;
     }
 
     @Override
+    @CircuitBreaker(name = "product-service", fallbackMethod = "fallback")
     public OrderResponse getOrderDetails(long orderId) {
         log.info("Get order details for order id {}.", orderId);
         Order order = orderRepository.findById(orderId)
@@ -94,26 +104,21 @@ public class OrderServiceImpl implements OrderService {
                         "NOT_FOUND",
                         404));
 
-        log.info("Calling to product service to fetch the product for id {}.", order.getProductId());
-        ProductDetails productDetails = webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .scheme("http")
-                        .host("localhost")
-                        .port(8080)
-                        .path("/product/{id}")
-                        .build(order.getProductId()))
-                .retrieve()
-                .onStatus(HttpStatusCode::isError, clientResponse -> clientResponse.bodyToMono(ErrorResponse.class)
-                        .flatMap(errorBody -> Mono.error(new OrderServiceCustomException(
-                                errorBody.getErrorMessage(),
-                                errorBody.getErrorCode(),
-                                clientResponse.statusCode().value()
-                        ))))
-                .bodyToMono(ProductDetails.class)
-                .block();
+        log.info("Calling the product service to fetch the product id {}.", order.getProductId());
+        ProductDetails productDetails = getProductDetails(order);
 
-        log.info("Calling to Payment service to fetch the payment for order id {}.", order.getId());
-        PaymentDetails paymentDetails = webClient.get()
+        log.info("Calling the Payment service to fetch the payment order id {}.", order.getId());
+        PaymentDetails paymentDetails = getPaymentDetails(order);
+
+        OrderResponse orderResponse = OrderMapper.INSTANCE.orderToOrderResponse(order);
+        //TODO: check how to to make the OrderMapper.INSTANCE assign the values.
+        orderResponse.setProductDetails(productDetails);
+        orderResponse.setPaymentDetails(paymentDetails);
+        return orderResponse;
+    }
+
+    private PaymentDetails getPaymentDetails(Order order) {
+        return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .scheme("http")
                         .host("localhost")
@@ -129,11 +134,34 @@ public class OrderServiceImpl implements OrderService {
                         ))))
                 .bodyToMono(PaymentDetails.class)
                 .block();
+    }
 
-        OrderResponse orderResponse = OrderMapper.INSTANCE.orderToOrderResponse(order);
-        //TODO: check how to to make the OrderMapper.INSTANCE assign the values.
-        orderResponse.setProductDetails(productDetails);
-        orderResponse.setPaymentDetails(paymentDetails);
-        return orderResponse;
+    private ProductDetails getProductDetails(Order order) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("http")
+                        .host("localhost")
+                        .port(8080)
+                        .path("/product/{id}")
+                        .build(order.getProductId()))
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> clientResponse.bodyToMono(ErrorResponse.class)
+                        .flatMap(errorBody -> Mono.error(new OrderServiceCustomException(
+                                errorBody.getErrorMessage(),
+                                errorBody.getErrorCode(),
+                                clientResponse.statusCode().value()
+                        ))))
+                .bodyToMono(ProductDetails.class)
+                .block();
+    }
+
+    private OrderResponse fallback(long orderId, WebClientRequestException ex) {
+        log.info(ex.getMessage());
+
+        throw new OrderServiceCustomException(
+                "Some service isn't accessible.",
+                "UNAVAILABLE",
+                500
+        );
     }
 }
